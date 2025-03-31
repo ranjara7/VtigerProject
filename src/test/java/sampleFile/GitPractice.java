aa46cd00bc32a8157cbdab05c2acd14ccd872581
Rajeev Ranjan <ranjara7@in.ibm.com>
2025-03-31 10:23:51 +0530

    commit from sample branch

---
+++ src/test/java/sampleFile/GitPractice.java
@@ -0,0 +1,11 @@
+package sampleFile;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Uploading changes from clone to github");
+
+	}
+
+}
